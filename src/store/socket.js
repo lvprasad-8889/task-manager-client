9ee26a09d12e63f701e3fd2e28659204c3ad541e
Lv Prasad <prasadlv8889@gmail.com>
2025-04-09 16:36:55 +0530

    set up ready for heroku deployment

--- src/store/socket.js
+++ src/store/socket.js
@@ -8,7 +8,7 @@ let socket;
 
 let prod = process.env.NODE_ENV !== 'development';
 if (!connect) {
-  socket = io(!prod ? "wss://task-managers-server-12a74ec3356d.herokuapp.com/" : "http://localhost:3000"); // Replace with your backend URL
+  socket = io(prod ? "wss://task-managers-server-12a74ec3356d.herokuapp.com/" : "http://localhost:3000"); // Replace with your backend URL
 }
 
 export default socket;
